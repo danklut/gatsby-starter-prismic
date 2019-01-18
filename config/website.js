55426e0bb471869da11c194f589604ad4b84e826
danklut <aomcloudenterprise@outlook.com>
2019-01-18 05:56:15 -0800

    Update website.js

--- config/website.js
+++ config/website.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Gatsby Starter - Prismic.io', // Navigation and Site Title
-  titleAlt: 'Gatsby Prismic.io', // Title for JSONLD
-  description: 'A typography-heavy & light-themed Gatsby Starter which uses the Headless CMS Prismic.',
-  headline: 'Writing and publishing content for LekoArts', // Headline for schema.org JSONLD
-  url: 'https://prismic.lekoarts.de', // Domain of your site. No trailing slash!
+  title: 'AOM Cloud Enterprise Group', // Navigation and Site Title
+  titleAlt: 'AOM Cloud Enterprise' // Title for JSONLD
+  description: 'The AOM Cloud Enterprise Blog is part of the AOM Cloud Enterprise Group.',
+  headline: 'Welcome to the AOM Cloud Enterprise Blog', // Headline for schema.org JSONLD
+  url: 'https://aomcloudenterprise.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   logo: '/logos/logo-1024.png', // Used for SEO
   ogLanguage: 'en_US', // Facebook Language
 
   // JSONLD / Manifest
   favicon: 'src/favicon.png', // Used for manifest favicon generation
-  shortName: 'Prismic', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  shortName: 'AOMCloud', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'DanKlut', // Author for schemaORGJSONLD
   themeColor: '#3D63AE',
   backgroundColor: '#EBEDF2',
 
